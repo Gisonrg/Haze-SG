5f64299271d40c1adf2e3f9c2846fabb66d31420
Jason Jiang <gisonrg@gmail.com>
2015-09-21 15:46:12 +0800

    Fix a bug when there is no login item in the system.

--- Haze@SG/LaunchStarter.swift
+++ Haze@SG/LaunchStarter.swift
@@ -13,25 +13,35 @@ func applicationIsInStartUpItems() -> Bool {
 }
 
 func itemReferencesInLoginItems() -> (existingReference: LSSharedFileListItemRef?, lastReference: LSSharedFileListItemRef?) {
-    if let appURL : NSURL = NSURL.fileURLWithPath(NSBundle.mainBundle().bundlePath) {
-        if let loginItemsRef = LSSharedFileListCreate(nil, kLSSharedFileListSessionLoginItems.takeRetainedValue(), nil).takeRetainedValue() as LSSharedFileListRef? {
-            
+
+    if let appUrl : NSURL = NSURL.fileURLWithPath(NSBundle.mainBundle().bundlePath) {
+        let loginItemsRef = LSSharedFileListCreate(
+            nil,
+            kLSSharedFileListSessionLoginItems.takeRetainedValue(),
+            nil
+            ).takeRetainedValue() as LSSharedFileListRef?
+        if loginItemsRef != nil {
             let loginItems: NSArray = LSSharedFileListCopySnapshot(loginItemsRef, nil).takeRetainedValue() as NSArray
-            let lastItemRef: LSSharedFileListItemRef = loginItems.lastObject as! LSSharedFileListItemRef
-            
-            for (index, _) in loginItems.enumerate() {
-                let currentItemRef: LSSharedFileListItemRef = loginItems.objectAtIndex(index) as! LSSharedFileListItemRef
-                if let itemURL = LSSharedFileListItemCopyResolvedURL(currentItemRef, 0, nil) {
-                    if (itemURL.takeRetainedValue() as NSURL).isEqual(appURL) {
-                        return (currentItemRef, lastItemRef)
+            if(loginItems.count > 0)
+            {
+                let lastItemRef: LSSharedFileListItemRef = loginItems.lastObject as! LSSharedFileListItemRef
+                for var i = 0; i < loginItems.count; ++i {
+                    let currentItemRef: LSSharedFileListItemRef = loginItems.objectAtIndex(i) as! LSSharedFileListItemRef
+                    if let itemURL = LSSharedFileListItemCopyResolvedURL(currentItemRef, 0, nil) {
+                        if (itemURL.takeRetainedValue() as NSURL).isEqual(appUrl) {
+                            return (currentItemRef, lastItemRef)
+                        }
                     }
                 }
+                
+                //The application was not found in the startup list
+                return (nil, lastItemRef)
+            } else {
+                let addatstart: LSSharedFileListItemRef = kLSSharedFileListItemBeforeFirst.takeRetainedValue()
+                return(nil,addatstart)
             }
-            
-            return (nil, lastItemRef)
         }
     }
-    
     return (nil, nil)
 }
 
