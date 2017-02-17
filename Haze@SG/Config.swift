5ff5917df3ecfd14793a9f61f1318c451d518969
Jason Jiang <gisonrg@gmail.com>
2017-02-17 21:52:10 +0800

    Remove API key

--- Haze@SG/Config.swift
+++ Haze@SG/Config.swift
@@ -10,5 +10,5 @@ import Foundation
 
 struct Config {
     // Register and get your API key on Data.gov at https://developers.data.gov.sg/environment/psi
-    static let apiKey = "ycHWlAJ18p4WnZpVAoeW8ko2ePvr2GeH"
+    static let apiKey = ""
 }
